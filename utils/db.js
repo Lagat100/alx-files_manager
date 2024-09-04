090e8a305c2d4e52d8250d5011886b60a5a0169f
Lagat100 <weldonkiprop1@gmail.com>
2024-09-04 21:14:17 +0000

    1. MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,64 @@
+import mongodb from 'mongodb';
+import envLoader from './env_loader';
+
+/**
+ * Represent a MongoDB client.
+ */
+class DBClient {
+  /**
+   * Creates an instance of DBClient.
+   */
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  /**
+   * Checks if the client's connection to the MongoDB Server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  /**
+   * Returns the number of users in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  /**
+   * Returns the number of files in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  /**
+   * Returns a reference to the `users` collection.
+   * @returns {Promise<Collection>}
+   */
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  /**
+   * Returns a reference to the `files` collection.
+   * @returns {Promise<Collection>}
+   */
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+export const client = new DBClient();
+export default client;
